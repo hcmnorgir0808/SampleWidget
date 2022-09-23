dda9ae67bf8e2187ed8757d786aeb75dc1ee258f
mocchan <hcmnorgir0808@yahoo.co.jp>
2022-09-23 20:43:59 +0900

    initial commit

---
+++ SampleWidget/SampleWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleWidgetApp.swift
+//  SampleWidget
+//
+//  Created by 岩本康孝 on 2022/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SampleWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
